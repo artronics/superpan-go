a0d3a97fc83a0939f19a99ae6655cfc45d5500e3
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-07-24 01:47:09 +0200

    composit medium to device but goc cyclic import. ready to delete device refs in medium

--- device/device.go
+++ device/device.go
@@ -1,17 +1,51 @@
 package device
 
-import "net"
+import (
+	"github.com/artronics/superpan/medium"
+	"net"
+)
 
 type Device struct {
-	Receiver
+	receiver
+	transmitter
+	medium     *medium.Medium
 	MACAddress MACAddress
 	DeviceType DeviceType
 }
 
-type MACAddress net.HardwareAddr
-
-type Receiver struct {
+type receiver struct {
 }
+
+type transmitter struct {
+	TX chan byte
+}
+
+func (t transmitter) Write(p []byte) (n int, err error) {
+	t.TX = make(chan byte, 1)
+	go func() {
+		for _, b := range p {
+			t.TX <- b
+		}
+		close(t.TX)
+	}()
+	n = 0
+
+	err = nil
+
+	return
+}
+
+func (d *Device) Start() {
+	go run()
+}
+
+func run() {
+	for {
+		return
+	}
+}
+
+type MACAddress net.HardwareAddr
 
 type DeviceType int
 
--- device/device_test.go
+++ device/device_test.go
@@ -1,1 +1,12 @@
 package device
+
+import "testing"
+
+func TestTransmitter_Write(t *testing.T) {
+	d := Device{}
+	d.Write([]byte{1, 2})
+	b := <-d.TX
+	if b != 2 {
+		t.Errorf("expected 2 items but got %d", b)
+	}
+}
